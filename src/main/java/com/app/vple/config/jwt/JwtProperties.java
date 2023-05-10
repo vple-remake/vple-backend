17993d4a7f47df7411e1e2dacb81133f2d68a55e
kimsh2948 <shkim2948@gmail.com>
2023-05-10 23:54:32 +0900

    feat: token 유효 시간 재 임시 변경

--- src/main/java/com/app/vple/config/jwt/JwtProperties.java
+++ src/main/java/com/app/vple/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.app.vple.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "{}";
-    int EXPIRATION_TIME =  86400;
+    int EXPIRATION_TIME =  8640000;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
